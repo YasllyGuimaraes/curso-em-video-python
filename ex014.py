b3d020e46fa9cdf16dd0e38714e71024dafbaf6f
Yaslly Guimarães <yaslly.guimaraes@hotmail.com>
2022-05-10 18:52:34 -0300

    Learning about arithmetic operators.

---
+++ ex014.py
@@ -0,0 +1,4 @@
+'''(014)Escreva um programa que converta uma temperatura digitada em °C e converta para °F.'''
+C = float(input('Informe a temperatura em °C: '))
+F = (C * (9 / 5)) +  32
+print(f'A temperatura de {C}°C corresponde a {F}°F')
